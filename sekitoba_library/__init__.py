07555e72133e19dc870eb1ff9b574c3fd1a64838
石川幹晴 <kansei2@kansei2.local>
2021-09-28 22:48:39 +0900

    脚質専用ファイルの追加

--- sekitoba_library/__init__.py
+++ sekitoba_library/__init__.py
@@ -4,3 +4,4 @@ from sekitoba_library.lib import *
 from sekitoba_library.past_race_data import *
 from sekitoba_library.connect import *
 from sekitoba_library.thread_scraping import *
+from sekitoba_library.limb import *
