10b762805d500ece113da004110a585f782d1a04
石川幹晴 <kansei@ishikawatsuyoshiharenoMac-mini.local>
2025-06-29 16:26:09 +0900

    update

--- SekitobaDataCreate/blood_type_score.py
+++ SekitobaDataCreate/blood_type_score.py
@@ -10,15 +10,21 @@ class BloodTypeScore:
         self.race_data = race_data
         self.horce_data = horce_data
 
-    def score_get( self, horce_id, cd: lib.CurrentData, pd: lib.PastData, getHorceData: GetHorceData ):
+    def score_get( self, horce_id, cd: lib.CurrentData, pd: lib.PastData, getHorceData: GetHorceData, sex = lib.escapeValue ):
         result = lib.escapeValue
         blood_type_score_data: dict = self.race_data.data[self.COLUM_NAME]
 
         if len( blood_type_score_data ) == 0:
             return result
-
+        
         horce_birth_day = int( horce_id[0:4] )
-        key_sex = str( int( self.horce_data.data[horce_id]["sex"] ) )
+        key_sex = ""
+        
+        if sex == lib.escapeValue:
+            key_sex = str( int( self.horce_data.data[horce_id]["sex"] ) )
+        else:
+            key_sex = str( int( sex ) )
+            
         key_age = str( int( int( self.race_data.data["year"] ) - horce_birth_day ) )
         key_interval = str( int( min( pd.race_interval(), 10 ) ) )
         key_limb = str( int( getHorceData.limb_math ) )
